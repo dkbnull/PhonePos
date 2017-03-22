7e3d3aa8407d7be25112179acd4da1824f158caa
dkbnull <dkbnull@163.com>
2017-03-22 21:58:53 +0800

    PhonePos 界面优化

--- www/js/controller/menu-controller.js
+++ www/js/controller/menu-controller.js
@@ -2,12 +2,18 @@ angular.module('menu.controller', ['menu.service'])
   .controller('menuCtrl', function ($scope, $state) {
     $scope.menuData = {};
 
-    $scope.menuData.username = localStorage.getItem('name');
+    if (localStorage.getItem('name') == '' || localStorage.getItem('name') == null ||
+      localStorage.getItem('name') == undefined) {
+      $scope.menuData.username = '';
+    } else {
+      $scope.menuData.username = localStorage.getItem('name');
+    }
 
     $scope.logout = function () {
       localStorage.removeItem('username');
       localStorage.removeItem('password');
       localStorage.removeItem('usercode');
+      localStorage.removeItem('name');
 
       $state.go('pos.login');
     }
